6c204b6ab78cbb2434430f598c7f515d296e1558
student <student>
2016-08-31 12:56:23 +0530

    Subtraction code done

--- addition.c
+++ addition.c
@@ -6,9 +6,9 @@ return n1 + n2;
 }
 
 
-int addition(int n1,int n2)
+int subtract(int n1,int n2)
 {
-return n1 + n2;
+return n1 - n2;
 }
 
 int addition(int n1,int n2)
@@ -26,18 +26,21 @@ void main()
 
 
 {
-	float num1,num2,sum;
+	float num1,num2,sum,diff,prod,div;
 	printf("Enter the first number: ");
 	scanf("%f",&num1);
 	printf("Enter the second number: ");
 	scanf("%f",&num2);
 	sum= addition(num1,num2);
 	printf("Sum is %f",sum);
-	 sum= addition(num1,num2);
-	 printf("Sum is %f",sum);
-	 sum= addition(num1,num2);
-	 printf("Sum is %f",sum);
-	 sum= addition(num1,num2);
-	 printf("Sum is %f",sum);
+	
+	diff= subtract(num1,num2);
+	printf("Difference is %f",diff);
+	
+	sum= addition(num1,num2);
+	printf("Sum is %f",sum);
+	
+	sum= addition(num1,num2);
+	printf("Sum is %f",sum);
 
 }
