eceeaf472378a567bdd94734845ebd12ad4058cd
student <student>
2016-08-31 12:52:21 +0530

    Addition code done

---
+++ addition.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int addition(int n1,int n2)
+{
+return n1 + n2;
+}
+
+
+int addition(int n1,int n2)
+{
+return n1 + n2;
+}
+
+int addition(int n1,int n2)
+{
+return n1 + n2;
+}
+
+int addition(int n1,int n2)
+{
+return n1 + n2;
+}
+
+
+void main()
+
+
+{
+	float num1,num2,sum;
+	printf("Enter the first number: ");
+	scanf("%f",&num1);
+	printf("Enter the second number: ");
+	scanf("%f",&num2);
+	sum= addition(num1,num2);
+	printf("Sum is %f",sum);
+	 sum= addition(num1,num2);
+	 printf("Sum is %f",sum);
+	 sum= addition(num1,num2);
+	 printf("Sum is %f",sum);
+	 sum= addition(num1,num2);
+	 printf("Sum is %f",sum);
+
+}
